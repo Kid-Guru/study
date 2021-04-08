0234948c2f1afb8e1d70f0180066b8d96f219823
Kid-Guru <birkov.alexandr@gmail.com>
2021-04-08 23:44:45 +0400

    done arrays exercies, activate test

--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -1,12 +1,33 @@
 module Exercise
   module Arrays
     class << self
+      def _find_max(array)
+        array.reduce { |max, el| el > max ? el : max }
+      end
+
       def replace(array)
-        array
+        max_elem = _find_max(array)
+        array.map { |el| el.positive? ? max_elem : el }
+      end
+
+      def _iter_search(array, query, left, rigth)
+        return -1 if left > rigth || left == array.length
+
+        middle = (rigth - left) / 2 + left
+
+        if array[middle] == query
+          middle
+        elsif array[middle] > query
+          _iter_search(array, query, 0, middle - 1)
+        else
+          _iter_search(array, query, middle + 1, rigth)
+        end
       end
 
       def search(_array, _query)
-        0
+        return -1 if _array.empty?
+
+        _iter_search(_array, _query, 0, _array.length)
       end
     end
   end
