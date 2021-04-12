48ce9fbf76266c6bb7e28670ab2d67cec38945a0
Kid-Guru <birkov.alexandr@gmail.com>
2021-04-12 19:50:35 +0400

    fix method names

--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -1,16 +1,16 @@
 module Exercise
   module Arrays
     class << self
-      def _find_max(array)
+      def find_max(array)
         array.reduce { |max, el| el > max ? el : max }
       end
 
       def replace(array)
-        max_elem = _find_max(array)
+        max_elem = find_max(array)
         array.map { |el| el.positive? ? max_elem : el }
       end
 
-      def _iter_search(array, query, left, rigth)
+      def iter_search(array, query, left, rigth)
         return -1 if left > rigth || left == array.length
 
         middle = (rigth - left) / 2 + left
@@ -18,16 +18,16 @@ module Exercise
         if array[middle] == query
           middle
         elsif array[middle] > query
-          _iter_search(array, query, 0, middle - 1)
+          iter_search(array, query, 0, middle - 1)
         else
-          _iter_search(array, query, middle + 1, rigth)
+          iter_search(array, query, middle + 1, rigth)
         end
       end
 
       def search(array, query)
         return -1 if array.empty?
 
-        _iter_search(array, query, 0, array.length)
+        iter_search(array, query, 0, array.length)
       end
     end
   end
